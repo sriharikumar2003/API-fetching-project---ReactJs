ac3f4b3539a2582738df961f21acd3ed8f54e8b5
Hari <sriharikumar.0903@gmail.com>
2024-08-17 11:55:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+import React from 'react';
+
 
 function App() {
+  const [Users,setUsers] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data =>{
+      setUsers(data);//fetching data from the users
+      setLoading(false);// initially it should be empty
+    });
+  },[]);
+
+  if(loading){
+    return <p>loading</p>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users and their IDs</h1>
+      <ul>
+        {Users.map(user => (
+          <React.Fragment key={user.id}>
+             <li className='id'><b>ID:</b> {user.id}</li>
+            <li className='name'><b>Name:</b> {user.name}</li>
+            <li>----------------------------------------</li>
+          </React.Fragment>
+        ))}
+      </ul>
     </div>
   );
-}
+} 
 
 export default App;
